1b6a125e4e9255dd030be20357b0dde0ab9a88c5
sarayu1201 <23P31A1201@acet.ac.in>
2024-10-14 19:10:20 +0530

    Uploading code: Fever.py

---
+++ Fever.py
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class EXamplle{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        if(x>98){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
